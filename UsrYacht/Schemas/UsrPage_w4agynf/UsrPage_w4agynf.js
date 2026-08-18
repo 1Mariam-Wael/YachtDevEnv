d6b91b0fe9dd289c685f0c322cf7ac0e758ebff5
1Mariam-Wael <2023170596@cis.asu.edu.eg>
2026-08-18 15:07:54 +0300

    contact age view

---
+++ UsrYacht/Schemas/UsrPage_w4agynf/UsrPage_w4agynf.js
@@ -0,0 +1,45 @@
+define("UsrPage_w4agynf", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"primaryDataSourceName": "UsrYachtDS"
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources"
+				],
+				"values": {
+					"UsrYachtDS": {
+						"type": "crt.EntityDataSource",
+						"scope": "page",
+						"config": {
+							"entitySchemaName": "UsrYacht",
+							"loadParameters": {
+								"options": {
+									"pagingConfig": {
+										"rowCount": 1,
+										"rowsOffset": -1
+									},
+									"sortingConfig": {
+										"columns": []
+									}
+								}
+							},
+							"allowCopyingRecords": false
+						}
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
